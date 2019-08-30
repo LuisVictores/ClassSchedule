bdd5db2804d8e0d5ad5fa7a72db325df7157fe98
Luis Victores <21384@columbushs.com>
2019-08-30 09:37:52 -0400

    First commit

---
+++ src/pkgclass/schedule/ClassSchedule.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.schedule;
+
+/**
+ *
+ * @author 21384
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String firstName = "Luis";
+        String lastName = "Victores";
+        int year = 2021;
+        System.out.println("My name is " + firstName + " " + lastName + " and i'll graduate in " + year);
+        System.out.println();
+        String class1 = "Math";
+        String teacher1 = "Maria Acevedo";
+        String class2 = "CSA";
+        String teacher2 = "Matthew Larrubia";
+        String class3 = "Physics";
+        String teacher3 = "Rudy Sanchez";
+        String class4 = "English";
+        String teacher4 = "Cody Morrissey";
+        String class5 = "APUSH";
+        String teacher5 = "Javier Hermida";
+        String class6 = "World Studies";
+        String teacher6 = "James Dugard";
+        String class7 = "Accounting";
+        String teacher7 = "James Moritz";
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("| 1 |                  " + class1 + "|         " + teacher1 + " |");
+        System.out.println("+--------------------------------------------------+");
+        
+        
+    }
+    
+}
